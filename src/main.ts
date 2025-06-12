720f788d343dc85252f1b4ab0bcb42f26871f574
Dan Aston <you@example.com>
2025-06-13 00:05:49 +1200

    Rendering posts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,37 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
+import techConference2025 from './posts/tech-conference-2025';
+import softwareEngineerInterview from './posts/software-engineer-interview';
+import latestSoftwareDevelopment from './posts/latest-software-development';
+import futureTechExhibition from './posts/future-tech-exhibition';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const blogPosts = [
+  techConference2025,
+  softwareEngineerInterview,
+  latestSoftwareDevelopment,
+  futureTechExhibition,
+];
+
+// Generate blog post HTML
+const generateBlogPostHTML = (post) => {
+  return post.visible ? `
+    <div class="blog-post">
+      <h2>${post.title}</h2>
+      <p>${post.content}</p>
+      <div class="tags">
+        ${post.tags.map(tag => `<span class="tag">${tag}</span>`).join(' ')}
+      </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+  ` : '';
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// Render blog posts
+const renderBlogPosts = () => {
+  const blogContainer = document.querySelector<HTMLDivElement>('#app');
+  blogContainer!.innerHTML = `
+    <div class="blog-wall">
+      ${blogPosts.map(generateBlogPostHTML).join('')}
+    </div>
+  `;
+};
+
+renderBlogPosts();
---
+++ src/posts/future-tech-exhibition.ts
@@ -0,0 +1,6 @@
+export default {
+  title: 'Doc Edge Film Festival 2025',
+  tags: ['exhibition'],
+  content: 'Insight into Doc Edge Film Festival...',
+  visible: true
+};
