62711589c91d4b0456c55e263fa8f9a9ad50e370
Dan Aston <you@example.com>
2025-06-14 15:56:52 +1200

    include dist

---
+++ dist/posts/foo.ts
@@ -0,0 +1,8 @@
+export default {
+  title: 'VMPW: Visual Music Performannce Workstation',
+  tags: ['software'],
+  content: `
+  Visual Music Performance Workstation is a new tool for VJs, aiming for the sweet spot between TouchDesigner and Resolume. 
+  ` ,
+  visible: true
+};
---
+++ dist/posts/postA.ts
@@ -0,0 +1,6 @@
+export default {
+  title: 'Interview with a Software Engineer',
+  tags: ['interview'],
+  content: 'Insights into the life of a software engineer...',
+  visible: false
+};
