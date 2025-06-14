2ec54bcf7d0578b3abfe22e7e3a2754fae8020d7
Dan Aston <you@example.com>
2025-06-14 15:51:42 +1200

    Updates

---
+++ posts/unfolding.ts
@@ -0,0 +1,9 @@
+export default {
+  title: 'Cable Management Presents: Unfolding',
+  tags: ['upcoming', 'event', 'ikona', 'nani'],
+  post_date: '2025-06-14',
+  content:
+    `Cable Management is the new event series hatched by Nani and Peter Ikona.
+  `,
+  visible: true
+};
